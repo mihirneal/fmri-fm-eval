cfde6d7ab13994974a976be7f4dd98903a832c51
Mihir Tripathy <45179700+mihirneal@users.noreply.github.com>
2026-01-17 10:13:44 -0600

    adding schaefer400_tians3_buckner7 to AABC (#43)

--- datasets/AABC/scripts/make_aabc_eval.sh
+++ datasets/AABC/scripts/make_aabc_eval.sh
@@ -6,6 +6,9 @@ spaces=(
     schaefer400_tians3
     flat
     a424
+    mni
+    mni_cortex
+    schaefer400_tians3_buckner7
 )
 
 log_path="logs/make_aabc_eval.log"
@@ -15,5 +18,6 @@ mkdir -p "$(dirname "${log_path}")"
 for space in "${spaces[@]}"; do
     uv run python datasets/AABC/scripts/make_aabc_eval.py \
         --space "${space}" \
+        --num_proc 8 \
         2>&1 | tee -a "${log_path}"
 done
